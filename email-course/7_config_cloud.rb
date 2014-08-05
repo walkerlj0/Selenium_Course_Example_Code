97f73c51f630924d82a6d70977d0c141f9b119e5
Dave Haeffner <dhaeffner@gmail.com>
2014-08-05 14:29:53 -0400

    Removed etraneous tips and consolidated two into one to make the final installment

--- email-course/7_config_cloud.rb
+++
@@ -1,9 +0,0 @@
-# filename: spec/spec_helper.rb
-
-ENV['base_url']         ||= 'http://the-internet.herokuapp.com'
-ENV['host']             = 'saucelabs'
-ENV['operating_system'] ||= 'Windows XP'
-ENV['browser']          ||= 'internet_explorer'
-ENV['browser_version']  ||= '8'
-ENV['SAUCE_USERNAME']   ||= 'your-sauce-username'
-ENV['SAUCE_ACCESS_KEY'] ||= 'your-sauce-access-key'
