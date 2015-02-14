d365fc97338f89a132b6e02e48b1769514b30670
Dave Haeffner <dhaeffner@gmail.com>
2015-02-13 19:02:42 -0500

    Broke ch 6 into 2 parts

---
+++ 06/01/src/test/java/tests/TestLogin.java
@@ -0,0 +1,32 @@
+package tests;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestLogin {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void succeeded() {
+        driver.get("http://the-internet.herokuapp.com/login");
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.id("login")).submit();
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
