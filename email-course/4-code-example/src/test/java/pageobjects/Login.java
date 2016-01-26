ea1e31c921eefaccfa13aa6a5d7e42827f4262d2
Dave Haeffner <dhaeffner@gmail.com>
2016-01-26 12:42:15 -0500

    Updated bootcamp email course

---
+++ email-course/4-code-example/src/test/java/pageobjects/Login.java
@@ -0,0 +1,36 @@
+package pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import static org.junit.Assert.assertTrue;
+
+public class Login extends Base {
+
+    By loginFormLocator = By.id("login");
+    By usernameLocator  = By.id("username");
+    By passwordLocator  = By.id("password");
+    By submitButton     = By.cssSelector("button");
+    By successMessageLocator = By.cssSelector(".flash.success");
+    By failureMessageLocator = By.cssSelector(".flash.error");
+
+    public Login(WebDriver driver) {
+        super(driver);
+        visit("http://the-internet.herokuapp.com/login");
+        assertTrue("The login form is not present",
+                isDisplayed(loginFormLocator));
+    }
+
+    public void with(String username, String password) {
+        type(username, usernameLocator);
+        type(password, passwordLocator);
+        click(submitButton);
+    }
+
+    public Boolean successMessagePresent() {
+        return isDisplayed(successMessageLocator);
+    }
+
+    public Boolean failureMessagePresent() {
+        return isDisplayed(failureMessageLocator);
+    }
+}
