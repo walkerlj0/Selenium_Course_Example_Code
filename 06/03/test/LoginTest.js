54ba93a2dc0c527267bff527b347a208e1c83954
Dave Haeffner <dhaeffner@gmail.com>
2016-12-05 00:30:03 -0500

    First pass on a working Selenium 3 setup with Firefox with both Martionette and Legacy FirefoxDriver

--- 06/03/test/LoginTest.js
+++ 06/03/test/LoginTest.js
@@ -2,12 +2,16 @@
 var webdriver = require('selenium-webdriver');
 var test = require('selenium-webdriver/testing');
 var assert = require('assert');
+var sleep = require('sleep');
 
 test.describe('Login', function() {
   this.timeout(30000);
   var driver;
 
   test.beforeEach(function() {
+    var vendorDirectory = process.cwd() + '/vendor';
+    process.env.PATH = vendorDirectory + ":$PATH";
+    //process.env.SELENIUM_MARIONETTE=false
     driver = new webdriver.Builder().forBrowser('firefox').build();
   });
 
@@ -19,7 +23,9 @@ test.describe('Login', function() {
     driver.get('http://the-internet.herokuapp.com/login');
     driver.findElement({id: 'username'}).sendKeys('tomsmith');
     driver.findElement({id: 'password'}).sendKeys('SuperSecretPassword!');
-    driver.findElement({css: 'button'}).click();
+    driver.findElement({css: 'button'}).click().then(function() {
+      sleep.sleep(1);
+    });
     driver.findElement({css: '.flash.success'}).isDisplayed().then(function(elementDisplayed) {
       assert.equal(elementDisplayed, true, 'Success message not displayed');
     });
