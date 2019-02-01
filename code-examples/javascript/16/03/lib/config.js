d958b997dce49624ea8e212a22af94698c33961b
David Haeffner <dhaeffner@gmail.com>
2019-02-01 21:06:44 +0200

    Cleaned up the config file a little

--- code-examples/javascript/16/03/lib/config.js
+++ code-examples/javascript/16/03/lib/config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  baseUrl: process.env.BASE_URL || 'http://the-internet.herokuapp.com',
-  host: process.env.HOST || 'localhost',
-  browser: process.env.BROWSER || 'chrome',
-  //browserVersion: process.env.BROWSER_VERSION || '11.0',
-  //platform: process.env.PLATFORM || 'Windows 7',
+  baseUrl: process.env.BASE_URL || "http://the-internet.herokuapp.com",
+  host: process.env.HOST || "localhost",
+  browser: process.env.BROWSER || "chrome",
+  // Sauce Config
   sauceUsername: process.env.SAUCE_USERNAME,
-  sauceAccessKey: process.env.SAUCE_ACCESS_KEY
+  sauceAccessKey: process.env.SAUCE_ACCESS_KEY,
+  browserVersion: process.env.BROWSER_VERSION || "11.0",
+  platform: process.env.PLATFORM || "Windows 7"
 };
 //module.exports = {
 //  baseUrl: process.env.BASE_URL || 'http://the-internet.herokuapp.com',
