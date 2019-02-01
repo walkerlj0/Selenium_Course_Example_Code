5b2090005fdcac71e542375a107a22cf034d912b
David Haeffner <dhaeffner@gmail.com>
2019-02-01 21:37:24 +0200

    Removed commented out code

--- code-examples/javascript/16/03/lib/config.js
+++ code-examples/javascript/16/03/lib/config.js
@@ -8,12 +8,3 @@ module.exports = {
   browserVersion: process.env.BROWSER_VERSION || "11.0",
   platform: process.env.PLATFORM || "Windows 7"
 };
-//module.exports = {
-//  baseUrl: process.env.BASE_URL || 'http://the-internet.herokuapp.com',
-//  host: process.env.HOST || 'saucelabs',
-//  browser: process.env.BROWSER || 'internet explorer',
-//  browserVersion: process.env.BROWSER_VERSION || '11.0',
-//  platform: process.env.PLATFORM || 'Windows 7',
-//  sauceUsername: process.env.SAUCE_USERNAME,
-//  sauceAccessKey: process.env.SAUCE_ACCESS_KEY
-//};
