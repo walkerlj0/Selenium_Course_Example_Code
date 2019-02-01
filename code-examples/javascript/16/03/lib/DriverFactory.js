22e270434f79a85ccb8842c7cafeadae6c6cd066
David Haeffner <dhaeffner@gmail.com>
2019-02-01 21:05:58 +0200

    Moved TODO comments to the top of the file. Renamed `configure` to `_configure` to denote its private usage. Swapped from if branches to switch/case in `_configure`. And updated the one-line if clause in `quit` to remove the open/closing brackets.

--- code-examples/javascript/16/03/lib/DriverFactory.js
+++ code-examples/javascript/16/03/lib/DriverFactory.js
@@ -1,33 +1,39 @@
 const { Builder } = require("selenium-webdriver");
 
+// TODO:
+// - Sauce Options
+// - Add OS detection or alt. options for Windows vs. POSIX for readers
+// - npm install chromedriver and geckodriver?
 class DriverFactory {
   constructor(config) {
     this.config = config;
   }
 
-  configure() {
+  _configure() {
     let builder;
-    if (this.config.host === "saucelabs") {
-      const url = "http://ondemand.saucelabs.com:80/wd/hub";
-      builder = new Builder().usingServer(url);
-      // TODO: Sauce Options
-      builder.withCapabilities({
-        browserName: this.config.browser,
-        browserVersion: this.config.browserVersion,
-        platform: this.config.platform,
-        username: this.config.sauceUsername,
-        accessKey: this.config.sauceAccessKey
-      });
-    } else if (this.config.host === "localhost") {
-      const vendorDirectory = process.cwd() + "/vendor";
-      process.env.PATH = vendorDirectory + ":$PATH"; // TODO: Add OS detection or alt. options for Windows vs. POSIX for readers
-      builder = new Builder().forBrowser(this.config.browser);
+    switch (this.config.host) {
+      case "saucelabs":
+        const url = "http://ondemand.saucelabs.com:80/wd/hub";
+        builder = new Builder().usingServer(url);
+        builder.withCapabilities({
+          browserName: this.config.browser,
+          browserVersion: this.config.browserVersion,
+          platform: this.config.platform,
+          username: this.config.sauceUsername,
+          accessKey: this.config.sauceAccessKey
+        });
+        break;
+      case "localhost":
+        var vendorDirectory = process.cwd() + "/vendor";
+        process.env.PATH = vendorDirectory + ":$PATH";
+        builder = new Builder().forBrowser(this.config.browser);
+        break;
     }
     return builder;
   }
 
   async build() {
-    this.driver = await this.configure().build();
+    this.driver = await this._configure().build();
     this.sessionId = await this.driver.getSession().id_;
     return this.driver;
   }
@@ -38,9 +44,8 @@ class DriverFactory {
       this.driver.executeScript("sauce:job-result=" + testResult);
     }
     await this.driver.quit();
-    if (this.config.host === "saucelabs" && testResult === false) {
+    if (this.config.host === "saucelabs" && testResult === false)
       throw new Error("https://saucelabs.com/beta/tests/" + this.sessionId);
-    }
   }
 }
 
