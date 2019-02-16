21e24896a4c9413cd8ca84901f2fa21ef21eda46
David Haeffner <dhaeffner@gmail.com>
2019-02-16 14:25:05 +0200

    Updated chapter 10 with latest examples

---
+++ code-examples/javascript/10/01/lib/selenium-util.js
@@ -0,0 +1,32 @@
+const Until = require('selenium-webdriver').until
+const config = require('./config')
+
+function setDriver(driver) {
+  this.driver = driver
+}
+
+async function visit(url) {
+  if (url.startsWith('http')) {
+    await this.driver.get(url)
+  } else {
+    await this.driver.get(config.baseUrl + url)
+  }
+}
+
+function find(locator) {
+  return this.driver.findElement(locator)
+}
+
+async function click(locator) {
+  await find(locator).click()
+}
+
+async function type(locator, inputText) {
+  await find(locator).sendKeys(inputText)
+}
+
+async function isDisplayed(locator) {
+  return await find(locator).isDisplayed()
+}
+
+module.exports = { setDriver, visit, find, click, type, isDisplayed }
---
+++ code-examples/javascript/10/02/lib/selenium-util.js
@@ -0,0 +1,36 @@
+const Until = require('selenium-webdriver').until
+const config = require('./config')
+
+function setDriver(driver) {
+  this.driver = driver
+}
+
+async function visit(url) {
+  if (url.startsWith('http')) {
+    await this.driver.get(url)
+  } else {
+    await this.driver.get(config.baseUrl + url)
+  }
+}
+
+function find(locator) {
+  return this.driver.findElement(locator)
+}
+
+async function click(locator) {
+  await find(locator).click()
+}
+
+async function type(locator, inputText) {
+  await find(locator).sendKeys(inputText)
+}
+
+async function isDisplayed(locator) {
+  try {
+    return await find(locator).isDisplayed()
+  } catch (error) {
+    return false
+  }
+}
+
+module.exports = { setDriver, visit, find, click, type, isDisplayed }
