3fb0927a7844ff4afd62ee52d5d19fc2bb3a439f
David Haeffner <dhaeffner@gmail.com>
2019-05-08 16:25:49 +0300

    Updated ch 16

--- code-examples/javascript/16/01/lib/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  baseUrl: process.env.BASE_URL || 'http://the-internet.herokuapp.com',
-  host: process.env.HOST || 'saucelabs',
-  browser: process.env.BROWSER || 'internet explorer',
-  browserVersion: process.env.BROWSER_VERSION || '11.0',
-  platform: process.env.PLATFORM || 'Windows 7',
-  sauceUsername: process.env.SAUCE_USERNAME,
-  sauceAccessKey: process.env.SAUCE_ACCESS_KEY
-};
---
+++ code-examples/javascript/16/test/DynamicLoadingTest.js
@@ -0,0 +1,29 @@
+require('./spec_helper')
+const assert = require('assert')
+const DynamicLoadingPage = require('../pages/DynamicLoadingPage')
+
+describe('Dynamic Loading @deep', function() {
+  let dynamicLoading
+
+  beforeEach(async function() {
+    dynamicLoading = new DynamicLoadingPage(this.driver)
+  })
+
+  it('hidden element', async function() {
+    await dynamicLoading.loadExample('1')
+    assert(
+      await dynamicLoading.isFinishTextPresent(),
+      true,
+      'Finish text not displayed'
+    )
+  })
+
+  it('rendered element', async function() {
+    await dynamicLoading.loadExample('2')
+    assert(
+      await dynamicLoading.isFinishTextPresent(),
+      true,
+      'Finish text not displayed'
+    )
+  })
+})
