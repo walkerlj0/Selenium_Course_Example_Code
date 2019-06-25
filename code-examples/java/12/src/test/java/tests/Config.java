ff352d08c5c158766a677a2e1a0993add95d7086
David Haeffner <dhaeffner@gmail.com>
2019-06-25 20:44:12 +0300

    Fixed code styles in config files, and fixed bad renames

--- code-examples/java/12/src/test/java/tests/Config.java
+++ code-examples/java/12/src/test/java/tests/Config.java
@@ -1,6 +1,6 @@
 package tests;
 
 public class Config {
-    public static final String baseUrl = System.getProperty("baseUrl", "http://the-internet.herokuapp.com");
-    public static final String browserName = System.getProperty("browserName", "firefox");
+    public static final String baseUrl      = System.getProperty("baseUrl", "http://the-internet.herokuapp.com");
+    public static final String browserName  = System.getProperty("browserName", "firefox");
 }
--- code-examples/java/15/02/src/test/java/tests/Config.java
+++ code-examples/java/15/02/src/test/java/tests/Config.java
@@ -2,10 +2,10 @@ package tests;
 
 public class Config {
     public static final String baseUrl        = System.getProperty("baseUrl", "http://the-internet.herokuapp.com");
-    public static final String browserName        = System.getProperty("browserName", "firefox");
+    public static final String browserName    = System.getProperty("browserName", "firefox");
     public static final String host           = System.getProperty("host", "localhost");
     public static final String browserVersion = System.getProperty("browserVersion", "50.0");
-    public static final String platformName       = System.getProperty("platformName", "Windows 10");
+    public static final String platformName   = System.getProperty("platformName", "Windows 10");
     public static final String sauceUser      = System.getenv("SAUCE_USERNAME");
     public static final String sauceKey       = System.getenv("SAUCE_ACCESS_KEY");
 }
