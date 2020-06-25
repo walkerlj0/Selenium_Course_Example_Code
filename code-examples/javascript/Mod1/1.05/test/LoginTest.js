25798f62f1c06e51cb78cf7265ecd32ab9d5f401
Lindsay Walker <lindsaywalker@sl-1134.attlocal.net>
2020-06-25 12:48:29 -0500

    Deleted the original code from ch06, added in code for lessons in Modules 1 & 2 of Selenium Course

---
+++ code-examples/javascript/Mod1/1.05/test/LoginTest.js
@@ -0,0 +1,18 @@
+const { Builder } = require('selenium-webdriver')
+const path = require('path')
+
+describe('Login', function() {
+  
+
+  beforeEach(async function() {
+    
+  })
+
+  afterEach(async function() {
+    
+  })
+
+  it('with valid credentials', async function() {
+    
+  })
+})
---
+++ code-examples/javascript/Mod2/2.05/test/LoginTest.js
@@ -0,0 +1,29 @@
+// filename: test/LoginTest.js
+const { Builder } = require('selenium-webdriver') 
+const path = require('path')
+
+//describe is a method from Mocha
+describe('Login', function() { 
+    this.timeout(30000)
+    let driver
+
+    beforeEach(async function() { 
+        /* const vendorDirectory =
+        path.delimiter + path.join(__dirname, '..', 'vendor') 
+        process.env.PATH += vendorDirectory */
+    driver = await new Builder().forBrowser('firefox').build()
+    })
+    afterEach(async function() { 
+        await driver.quit()
+    })
+    it('with valid credentials', async function() {
+        await driver.get('http://the-internet.herokuapp.com/login') 
+        await driver
+            .findElement({ id: 'username' })
+            .sendKeys('tomsmith') 
+        await driver
+            .findElement({ id: 'password' })
+            .sendKeys('SuperSecretPassword!')
+        await driver.findElement({ css: 'button' }).click()
+    }) 
+})
