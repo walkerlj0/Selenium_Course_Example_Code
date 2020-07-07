3b307da4fef18ab49798dfaf8c20706def2589d5
Lindsay Walker <lindsaywalker@sl-1134.attlocal.net>
2020-07-07 14:04:22 -0500

    Added code for 3.06 JS where user creates an runs two tests in DynamicLoadingPage.js

---
+++ code-examples/javascript/Mod3/3.06/test/LoginTest.js
@@ -0,0 +1,41 @@
+// filename: test/LoginTest.js
+const { Builder } = require('selenium-webdriver') 
+const path = require('path')
+const assert = require('assert')
+const LoginPage = require('../pages/LoginPage')
+
+
+//describe is a method from Mocha
+describe('Login', function() { 
+    this.timeout(30000)
+    let driver 
+    let login
+
+    beforeEach(async function() { 
+    driver = await new Builder().forBrowser('firefox').build()
+    login = new LoginPage(driver)
+    await login.load()
+    })
+
+    afterEach(async function() { 
+        await driver.quit()
+    })
+
+    it('with valid credentials', async function() {
+    await login.authenticate('tomsmith', 'SuperSecretPassword!')
+    assert(await login.successMessagePresent(), 'Success message not displayed')
+  })
+  
+  it('with invalid credentials', async function() {
+    await login.authenticate('tomsmith', 'SuperSecretPassword!')
+    assert(!(await login.failureMessagePresent()), 'Failure message is not displayed')
+    // assert(!(await login.successMessagePresent()), 'Success message not displayed')
+  })
+  it('rendered element', async function() {
+    await dynamicLoading.loadExample('2')
+    assert(await dynamicLoading.isFinishTextPresent(), true, 'Finish text not displayed'
+    )
+  })
+
+
+})
