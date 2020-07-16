053da75095121c612bcaccc2c67819eecc48fc93
Lindsay Walker <lindsaywalker@sl-1134.attlocal.net>
2020-07-16 14:14:24 -0500

    updated 4.03 so it runs with the browser specified in config.js by requiring config in DriverFactory.js

--- javascript/Mod4/4.03/lib/DriverFactory.js
+++ javascript/Mod4/4.03/lib/DriverFactory.js
@@ -1,11 +1,12 @@
 // filename: lib/DriverFactory.js
 //const path = require('path')
+const config = require('../lib/config')
 const { Builder } = require('selenium-webdriver')
 
 class DriverFactory {
   async build() {
     //process.env.PATH += path.delimiter + path.join(__dirname, '..', 'vendor')
-    this.driver = await new Builder().forBrowser(this.config.browser).build()
+    this.driver = await new Builder().forBrowser(config.browser).build()
   }
 
   async quit() {
--- javascript/Mod4/4.03/test/LoginTest.js
+++ javascript/Mod4/4.03/test/LoginTest.js
@@ -1,6 +1,5 @@
 // filename: test/LoginTest.js
 require('./spec_helper')
-//const { Builder } = require('selenium-webdriver') 
 //const path = require('path')
 const assert = require('assert')
 const LoginPage = require('../pages/LoginPage')
@@ -8,19 +7,12 @@ const LoginPage = require('../pages/LoginPage')
 
 //describe is a method from Mocha
 describe('Login', function() { 
-    //this.timeout(30000)
-    //let driver 
-    let login
 
+    let login
     beforeEach(async function() { 
-    //driver = await new Builder().forBrowser('firefox').build()
     login = new LoginPage(this.driver)
     await login.load()
     })
-
-    // afterEach(async function() { 
-    //     await driver.quit()
-    // })
 
     it('with valid credentials', async function() {
     await login.authenticate('tomsmith', 'SuperSecretPassword!')
@@ -30,7 +22,6 @@ describe('Login', function() {
     it('not invalid credentials', async function() {
       await login.authenticate('tomsmith', 'SuperSecretPassword!')
       assert(!(await login.failureMessagePresent()), 'Failure message displayed')
-      //assert(!(await login.successMessagePresent()), 'Success message displayed')
     })
 
 })
