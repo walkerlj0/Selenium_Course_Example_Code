64b901e24f2078d2571e7de0cdbcbafee510e405
Lindsay Walker <lindsaywalker@sl-1134.attlocal.net>
2020-07-29 17:23:56 -0500

    added 2.06 having trouble with chrome and can't get mvn clean test to run

---
+++ java/Mod1/SeleniumJava copy/src/test/java/companyname/TestLogin.java
@@ -0,0 +1,43 @@
+//filename: tests/TestLogin.java
+package companyname;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import org.openqa.selenium.By;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeBrowser;
+
+public class TestLogin {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+//        System.setProperty("webdriver.gecko.driver",
+//                System.getProperty("user.dir") + "/vendor/geckodriver");
+        MutableCapabilities sauceOptions = new MutableCapabilities();
+        ChromeOptions browserOptions = new ChromeOptions();
+        browserOptions.setExperimentalOption("w3c", true);
+        browserOptions.setCapability("platformName", "macOS 10.15");
+        browserOptions.setCapability("browserVersion", "latest-1");
+        browserOptions.setCapability("sauce:options", sauceOptions);
+    }
+
+    @Test
+    public void succeeded() {
+        driver.get("http://the-internet.herokuapp.com/login");
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.cssSelector("button")).click();
+        assertTrue("success message not present",
+                driver.findElement(By.cssSelector(".flash.success")).isDisplayed());
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
