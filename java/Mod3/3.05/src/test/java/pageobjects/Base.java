98519c19dea63a7e4998f514a85c5b3982b4bcfc
Lindsay Walker <lindsay.waklker@saucelabs.com>
2020-08-31 13:44:17 -0500

    Added 3.05 example code

---
+++ java/Mod3/3.05/src/test/java/pageobjects/Base.java
@@ -0,0 +1,40 @@
+
+// filename: pageobjects/Base.java
+package pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Base {
+
+    private WebDriver driver;
+
+    public Base(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void visit(String url) {
+        driver.get(url);
+    }
+
+    public WebElement find(By locator) {
+        return driver.findElement(locator);
+    }
+
+    public void click(By locator) {
+        find(locator).click();
+    }
+
+    public void type(String inputText, By locator) {
+        find(locator).sendKeys(inputText);
+    }
+
+    public Boolean isDisplayed(By locator) {
+        try {
+            return find(locator).isDisplayed();
+        } catch (org.openqa.selenium.NoSuchElementException exception) {
+            return false;
+        }
+    }
+}
