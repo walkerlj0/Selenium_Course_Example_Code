117751f7b4584c25236b7f86fb51f74c9ee97cd5
Lindsay Walker <lindsay.waklker@saucelabs.com>
2020-09-14 15:43:32 -0500

    Added code for 4.05 Java

---
+++ java/Mod4/4.05/src/test/java/tests/Config.java
@@ -0,0 +1,12 @@
+// filename: tests/Config.java
+package tests;
+
+public class Config {
+    public static final String baseUrl = System.getProperty("baseUrl", "http://the-internet.herokuapp.com");
+    public static final String host = System.getProperty("host", "saucelabs");
+    public static final String browserName = System.getProperty("browserName", "chrome");
+    public static final String browserVersion = System.getProperty("browserVersion", "75.0");
+    public static final String platformName = System.getProperty("platformName", "Windows 10");
+    public static final String sauceUser = System.getenv("SAUCE_USERNAME");
+    public static final String sauceKey = System.getenv("SAUCE_ACCESS_KEY");
+}
