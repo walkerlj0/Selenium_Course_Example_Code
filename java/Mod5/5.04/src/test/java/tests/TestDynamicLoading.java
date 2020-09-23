2895c470e36336857f853301cbcec5b7000f219d
Lindsay Walker <lindsay.waklker@saucelabs.com>
2020-09-23 08:51:06 -0500

    added test code for java 5.04 on running parallel, random tests.

---
+++ java/Mod5/5.04/src/test/java/tests/TestDynamicLoading.java
@@ -0,0 +1,32 @@
+// filename: tests/TestDynamicLoading.java
+package tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import pageobjects.DynamicLoading;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestDynamicLoading extends BaseTest {
+
+    private DynamicLoading dynamicLoading;
+
+    @Before
+    public void setUp() {
+        dynamicLoading = new DynamicLoading(driver);
+    }
+
+    @Test
+    public void hiddenElementLoads() {
+        dynamicLoading.loadExample("1");
+        assertTrue("finish text didn't display after loading",
+                dynamicLoading.finishTextPresent());
+    }
+
+    @Test
+    public void elementAppears() {
+        dynamicLoading.loadExample("2");
+        assertTrue("finish text didn't render after loading",
+                dynamicLoading.finishTextPresent());
+    }
+}
