ad99d98ec68fb550c6b214beff7115b9e3253974
Lindsay Walker <lindsay.waklker@saucelabs.com>
2020-10-08 15:42:12 -0500

    Updated Module 4 with new W3C capabilties

--- javascript/Mod4/4.06/lib/config.js
+++ javascript/Mod4/4.06/lib/config.js
@@ -1,13 +1,14 @@
 // filename: lib/config.js
 module.exports = {
   baseUrl: process.env.BASE_URL || 'http://the-internet.herokuapp.com',
-  browser: process.env.BROWSER || 'firefox',
   host: process.env.HOST || 'saucelabs',
   sauce: {
-    username: process.env.SAUCE_USERNAME,
-    accessKey: process.env.SAUCE_ACCESS_KEY,
-    browserName: process.env.BROWSER_NAME || 'internet explorer',
-    browserVersion: process.env.BROWSER_VERSION || '11.0',
-    platformName: process.env.PLATFORM_NAME || 'Windows 7',
+    "browserName": process.env.BROWSER_NAME || 'chrome',
+    "browserVersion": process.env.BROWSER_VERSION || '75.0',
+    "platformName": process.env.PLATFORM_NAME || 'Windows 10',
+    "sauce:options": {
+      "username": process.env.SAUCE_USERNAME,
+      "accessKey": process.env.SAUCE_ACCESS_KEY,
+    }
   },
 }
