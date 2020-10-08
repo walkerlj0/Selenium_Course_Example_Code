26910d52c9d4bc9920d0c50520167cabdca30465
Lindsay Walker <lindsay.waklker@saucelabs.com>
2020-10-08 15:44:47 -0500

    updated all module 5 config.js with updated W3C capabilities

--- javascript/Mod5/5.05/lib/config.js
+++ javascript/Mod5/5.05/lib/config.js
@@ -1,15 +1,16 @@
 // filename: lib/config.js
 module.exports = {
   baseUrl: process.env.BASE_URL || 'http://the-internet.herokuapp.com',
-  browser: process.env.BROWSER || 'firefox',
-  host: process.env.HOST || 'saucelabs',
-  sauce: {
-    username: process.env.SAUCE_USERNAME,
-    accessKey: process.env.SAUCE_ACCESS_KEY,
-    browserName: process.env.BROWSER_NAME || 'internet explorer',
-    browserVersion: process.env.BROWSER_VERSION || '11.0',
-    platformName: process.env.PLATFORM_NAME || 'Windows 7',
-  },
+host: process.env.HOST || 'saucelabs',
+sauce: {
+  "browserName": process.env.BROWSER_NAME || 'chrome',
+  "browserVersion": process.env.BROWSER_VERSION || '75.0',
+  "platformName": process.env.PLATFORM_NAME || 'Windows 10',
+  "sauce:options": {
+    "username": process.env.SAUCE_USERNAME,
+    "accessKey": process.env.SAUCE_ACCESS_KEY,
+  }
+},
   sauceW3C: {
     "browserName": process.env.BROWSER_NAME || 'chrome',
     "browserVersion": process.env.BROWSER_VERSION || "75.0",
@@ -20,5 +21,4 @@ module.exports = {
       "tunnelIdentifier": process.env.SAUCE_TUNNEL,
     }
   },
-
 }
--- javascript/Mod5/5.07/lib/config.js
+++ javascript/Mod5/5.07/lib/config.js
@@ -1,15 +1,16 @@
 // filename: lib/config.js
 module.exports = {
-  baseUrl: process.env.BASE_URL || 'http://the-internet.herokuapp.com',
-  browser: process.env.BROWSER || 'firefox',
-  host: process.env.HOST || 'ondemand',
-  sauce: {
-    username: process.env.SAUCE_USERNAME,
-    accessKey: process.env.SAUCE_ACCESS_KEY,
-    browserName: process.env.BROWSER_NAME || 'internet explorer',
-    browserVersion: process.env.BROWSER_VERSION || '11.0',
-    platformName: process.env.PLATFORM_NAME || 'Windows 7',
-  },
+baseUrl: process.env.BASE_URL || 'http://the-internet.herokuapp.com',
+host: process.env.HOST || 'saucelabs',
+sauce: {
+  "browserName": process.env.BROWSER_NAME || 'chrome',
+  "browserVersion": process.env.BROWSER_VERSION || '75.0',
+  "platformName": process.env.PLATFORM_NAME || 'Windows 10',
+  "sauce:options": {
+    "username": process.env.SAUCE_USERNAME,
+    "accessKey": process.env.SAUCE_ACCESS_KEY,
+  }
+},
   sauceW3C: {
     "browserName": process.env.BROWSER_NAME || 'chrome',
     "browserVersion": process.env.BROWSER_VERSION || "75.0",
