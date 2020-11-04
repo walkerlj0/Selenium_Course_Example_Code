ce89c2b7f3145d4e24ff187d58715cbd2854dcdf
Lindsay Walker <lindsay.waklker@saucelabs.com>
2020-11-04 16:41:04 -0600

    added final code for Python 3.04

---
+++ python/Mod3/3.04/pages/login_page.py
@@ -0,0 +1,36 @@
+# filename: pages/login_page.py
+from selenium.webdriver.common.by import By
+
+class LoginPage():
+    _username_input = {"by": By.ID, "value": "username"}
+    _password_input = {"by": By.ID, "value": "password"}
+    _submit_button = {"by": By.CSS_SELECTOR, "value": "button"}
+    _success_message = {"by": By.CSS_SELECTOR, "value": ".flash.success"}
+    _failure_message = {"by": By.CSS_SELECTOR, "value": ".flash.error"}
+    _login_form = {"by": By.ID, "value": "login"}
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.driver.get("http://the-internet.herokuapp.com/login")
+        assert self.driver.find_element(
+            self._login_form["by"], self._login_form["value"]).is_displayed()
+
+    def with_(self, username, password):
+        self.driver.find_element(self._username_input["by"],
+                                 self._username_input["value"]).send_keys(username)
+        self.driver.find_element(self._password_input["by"],
+                                 self._password_input["value"]).send_keys(password)
+        self.driver.find_element(self._submit_button["by"],
+                                 self._submit_button["value"]).click()
+
+    def success_message_present(self):
+        return self.driver.find_element(
+            self._success_message["by"], self._success_message["value"]).is_displayed()
+
+    def failure_message_present(self):
+        return self.driver.find_element(
+            self._failure_message["by"], self._failure_message["value"]).is_displayed()
+
+    def test_invalid_credentials(self, login):
+        login.with_("tomsmith", "bad password")
+        assert login.failure_message_present()
---
+++ python/Mod3/3.04/tests/login_test.py
@@ -0,0 +1,29 @@
+# filename: tests/login_test.py
+import pytest
+import os
+from selenium import webdriver
+from pages.login_page import LoginPage
+
+@pytest.fixture
+def login(request):
+    _chromedriver = os.path.join(os.getcwd(), 'vendor', 'chromedriver')
+    if os.path.isfile(_chromedriver):
+        driver_ = webdriver.Chrome(_chromedriver)
+
+    else:
+        driver_ = webdriver.Chrome()
+
+    loginPage = LoginPage(driver_)
+    def quit():
+        driver_.quit()
+
+    request.addfinalizer(quit)
+    return loginPage
+
+def test_valid_credentials(login):
+    login.with_("tomsmith", "SuperSecretPassword!")
+    assert login.success_message_present()
+
+def test_invalid_credentials(login):
+    login.with_("tomsmith", "bad password")
+    assert login.failure_message_present()
