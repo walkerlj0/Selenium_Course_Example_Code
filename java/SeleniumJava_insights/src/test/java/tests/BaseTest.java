e73752494c9800fe08e297e852e4d967a6a86603
Diego Molina <diemol@gmail.com>
2020-11-13 22:36:59 +0100

    Refactoring to use browser options
    Also, creating a single MutableCapabilities object to
    avoid duplicating code.

--- java/SeleniumJava_insights/src/test/java/tests/BaseTest.java
+++ java/SeleniumJava_insights/src/test/java/tests/BaseTest.java
@@ -32,53 +32,77 @@ public class BaseTest {
 
         @Override
         protected void before() throws Exception {
-            if (host.equals("saucelabs")) {
-                MutableCapabilities sauceOptions = new MutableCapabilities();
-                sauceOptions.setCapability("username", sauceUser);
-                sauceOptions.setCapability("accessKey", sauceKey);
-                sauceOptions.setCapability("name", testName);
-                sauceOptions.setCapability("extendedDebugging", "true");
-                sauceOptions.setCapability("capturePerformance","true");
-                MutableCapabilities capabilities = new MutableCapabilities();
-                capabilities.setCapability("browserName", browserName);
-                capabilities.setCapability("browserVersion", browserVersion);
-                capabilities.setCapability("platformName", platformName);
-                capabilities.setCapability("sauce:options", sauceOptions);
-                String sauceUrl = String.format("https://ondemand.us-west-1.saucelabs.com/wd/hub");
-                driver = new RemoteWebDriver(new URL(sauceUrl), capabilities);
-                sessionId = ((RemoteWebDriver) driver).getSessionId().toString();
-                System.out.printf("Started %s", new Date().toString());
-                System.out.printf("SauceOnDemandSessionID=%s job-name=%s", sessionId, testName);
-                sauceClient = new SauceREST(sauceUser, sauceKey, DataCenter.US);
-
-            } else if (host.equals("localhost")) {
-                if (browserName.equals("firefox")) {
-                    System.setProperty("webdriver.gecko.driver",
-                            System.getProperty("webdriver.gecko.driver", "src/test/java/drivers/geckodriver"));
-                    driver = new FirefoxDriver();
-                } else if (browserName.equals("chrome")) {
-                    System.setProperty("webdriver.chrome.driver", "src/test/java/drivers/chromedriver");
-                    ChromeOptions browserOptions = new ChromeOptions();
-                    driver = new ChromeDriver();
+            String sauceUrl = "https://ondemand.us-west-1.saucelabs.com/wd/hub";
+            MutableCapabilities capabilities;
+            switch (browserName) {
+                case BrowserType.SAFARI: {
+                    capabilities = new SafariOptions();
+                    break;
+                }
+                case BrowserType.FIREFOX: {
+                    capabilities = new FirefoxOptions();
+                    break;
+                }
+                case BrowserType.IE: {
+                    capabilities = new InternetExplorerOptions();
+                    break;
+                }
+                case BrowserType.EDGE: {
+                    capabilities = new EdgeOptions();
+                    break;
+                }
+                default: {
+                    capabilities = new ChromeOptions();
+                    break;
                 }
             }
-            else if (host.equals("saucelabs-tunnel")) {
-                MutableCapabilities sauceOptions = new MutableCapabilities();
-                sauceOptions.setCapability("username", sauceUser);
-                sauceOptions.setCapability("accessKey", sauceKey);
-                sauceOptions.setCapability("name", testName);
-                sauceOptions.setCapability("tunnelIdentifier", sauceTunnel);
-                MutableCapabilities capabilities = new MutableCapabilities();
-                capabilities.setCapability("browserName", browserName);
-                capabilities.setCapability("browserVersion", browserVersion);
-                capabilities.setCapability("platformName", platformName);
-                capabilities.setCapability("sauce:options", sauceOptions);
-                String sauceUrl = String.format("https://ondemand.us-west-1.saucelabs.com/wd/hub");
-                driver = new RemoteWebDriver(new URL(sauceUrl), capabilities);
-                sessionId = ((RemoteWebDriver) driver).getSessionId().toString();
-                System.out.printf("Started %s", new Date().toString());
-                System.out.printf("SauceOnDemandSessionID=%s job-name=%s", sessionId, testName);
-                sauceClient = new SauceREST(sauceUser, sauceKey, DataCenter.US);
+            capabilities.setCapability("browserVersion", browserVersion);
+            capabilities.setCapability("platformName", platformName);
+
+            MutableCapabilities sauceOptions = new MutableCapabilities();
+            sauceOptions.setCapability("username", sauceUser);
+            sauceOptions.setCapability("accessKey", sauceKey);
+            sauceOptions.setCapability("name", testName);
+
+            switch (host) {
+                case "saucelabs": {
+                    sauceOptions.setCapability("extendedDebugging", "true");
+                    sauceOptions.setCapability("capturePerformance", "true");
+                    capabilities.setCapability("sauce:options", sauceOptions);
+
+                    driver = new RemoteWebDriver(new URL(sauceUrl), capabilities);
+                    sessionId = ((RemoteWebDriver) driver).getSessionId().toString();
+                    System.out.printf("Started %s", new Date().toString());
+                    System.out.printf("SauceOnDemandSessionID=%s job-name=%s", sessionId, testName);
+                    sauceClient = new SauceREST(sauceUser, sauceKey, DataCenter.US);
+
+                    break;
+                }
+                case "localhost":
+                    if ("firefox".equals(browserName)) {
+                        System.getProperty("webdriver.gecko.driver",
+                                           "src/test/java/drivers/geckodriver");
+                        System.setProperty("webdriver.gecko.driver",
+                                           System.getProperty("webdriver.gecko.driver",
+                                                              "src/test/java/drivers/geckodriver"));
+                        driver = new FirefoxDriver();
+                    } else if ("chrome".equals(browserName)) {
+                        System.setProperty("webdriver.chrome.driver",
+                                           "src/test/java/drivers/chromedriver");
+                        driver = new ChromeDriver();
+                    }
+                    break;
+                case "saucelabs-tunnel": {
+                    sauceOptions.setCapability("tunnelIdentifier", sauceTunnel);
+                    capabilities.setCapability("sauce:options", sauceOptions);
+
+                    driver = new RemoteWebDriver(new URL(sauceUrl), capabilities);
+                    sessionId = ((RemoteWebDriver) driver).getSessionId().toString();
+                    System.out.printf("Started %s", new Date().toString());
+                    System.out.printf("SauceOnDemandSessionID=%s job-name=%s", sessionId, testName);
+                    sauceClient = new SauceREST(sauceUser, sauceKey, DataCenter.US);
+                    break;
+                }
             }
         }
 
@@ -91,7 +115,6 @@ public class BaseTest {
 
     @Rule
     public TestRule watcher;
-
     {
         watcher = new TestWatcher() {
             @Override
@@ -101,14 +124,15 @@ public class BaseTest {
 
             @Override
             protected void failed(Throwable throwable, Description description) {
-            if (host.equals("saucelabs")) {
-                sauceClient.jobFailed(sessionId);
-                System.out.println(String.format("https://saucelabs.com/tests/%s", sessionId));
+                if ("saucelabs".equals(host)) {
+                    sauceClient.jobFailed(sessionId);
+                    System.out.println(String.format("https://saucelabs.com/tests/%s", sessionId));
+                }
             }
-        }
+
             @Override
             protected void succeeded(Description description) {
-                if (host.equals("saucelabs")) {
+                if ("saucelabs".equals(host)) {
                     sauceClient.jobPassed(sessionId);
                 }
             }
