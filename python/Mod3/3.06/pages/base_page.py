47741ffd205b2c69c30d0b1425a76b21798a0e42
Lindsay Walker <lindsay.waklker@saucelabs.com>
2021-01-06 15:09:53 -0600

    updated 3.06

---
+++ python/Mod3/3.06/pages/base_page.py
@@ -0,0 +1,34 @@
+#  filename: pages/base_page.py
+from selenium.common.exceptions import NoSuchElementException
+
+class BasePage():
+    def __init__(self, driver):
+        self.driver = driver
+
+    def _visit(self, url):
+        self.driver.get(url)
+
+    def _find(self, locator):
+        return self.driver.find_element(locator["by"], locator["value"])
+
+    def _click(self, locator):
+        self._find(locator).click()
+
+    def _type(self, locator, input_text):
+        self._find(locator).send_keys(input_text)
+
+    def _is_displayed(self, locator, timeout=0):
+        if timeout > 0:
+            try:
+                wait = WebDriverWait(self.driver, timeout)
+                wait.until(
+                    expected_conditions.visibility_of_element_located(
+                        (locator['by'], locator['value'])))
+            except TimeoutException:
+                return False
+            return True
+        else:
+            try:
+                return self._find(locator).is_displayed()
+            except NoSuchElementException:
+                return False
