734079d635e6d41f6da103cc2f17ecf97bedf9ef
Matt Rogers <marogers@tandemdiabetes.com>
2021-09-28 10:06:04 -0700

    Completed Module two of the saucelabs tutorial

--- javascript/Mod1/1.05/test/LoginTest.js
+++ javascript/Mod1/1.05/test/LoginTest.js
@@ -1,18 +1,34 @@
 const { Builder } = require('selenium-webdriver')
 const path = require('path')
+const assert = require('assert')
 
-describe('Login', function() {
-  
+//describe is a method from Mocha
+describe('Login', function () {
+  this.timeout(30000)
+  let driver
 
-  beforeEach(async function() {
-    
+  beforeEach(async function () {
+    /* const vendorDirectory =
+    path.delimiter + path.join(__dirname, '..', 'vendor') 
+    process.env.PATH += vendorDirectory */
+    driver = await new Builder().forBrowser('firefox').build();
   })
-
-  afterEach(async function() {
-    
+  afterEach(async function () {
+    await driver.quit()
   })
-
-  it('with valid credentials', async function() {
-    
+  it('with valid credentials', async function () {
+    await driver.get('http://the-internet.herokuapp.com/login')
+    await driver
+      .findElement({ id: 'username' })
+      .sendKeys('tomsmith')
+    await driver
+      .findElement({ id: 'password' })
+      .sendKeys('SuperSecretPassword!')
+    await driver.findElement({ css: 'button' }).click()
+    assert(
+      await driver.findElement({
+        css: '.flash.success'
+      }).isDisplayed(), 'Success message not displayed'
+    )
   })
 })
